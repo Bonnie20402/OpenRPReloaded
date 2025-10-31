5d06131ae41bafa47e7f6a16f150e695ee993a1c
Bonnie20402 <49130379+Bonnie20402@users.noreply.github.com>
2025-10-31 16:01:35 +0000

    Sistema de Contas [WIP]

--- OpenRPReloaded/Services/AccountsService.cs
+++ OpenRPReloaded/Services/AccountsService.cs
@@ -1,7 +1,10 @@
 ﻿using OpenRPReloaded.Contexts;
+using OpenRPReloaded.Enums;
 using OpenRPReloaded.Models;
+using SampSharp.Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OpenRPReloaded.Services
@@ -9,12 +12,12 @@ namespace OpenRPReloaded.Services
     /// <summary>
     /// Responsavel pela gestão das contas do servidor.
     /// </summary>
-    public class LoginService
+    public class AccountsService
     {
 
         private AccountsContext _accountsContext;
 
-        public LoginService(AccountsContext accountsContext)
+        public AccountsService(AccountsContext accountsContext)
         {
             _accountsContext = accountsContext;
         }
@@ -25,9 +28,76 @@ namespace OpenRPReloaded.Services
         /// </summary>
         /// <param name="username"> O username do jogador. </param>
         /// <returns></returns>
-        public static bool IsRegistered(string username)
+        public bool IsRegistered(string username)
         {
-            Account account = _ac
+            return _accountsContext.Accounts.Any(a => a.Username == username);
         }
+
+
+        /// <summary>
+        /// Registra uma conta, e guarda-a na BD.
+        /// </summary>
+        /// <param name="username">O username</param>
+        /// <param name="passwordUnhashed">A password (unhashed)</param>
+        /// <returns></returns>
+        public List<AccountCreationResult> RegisterAccount(string username, string passwordUnhashed)
+        {
+            List<AccountCreationResult> results = new List<AccountCreationResult>();
+
+            // O username só pode ter até 31 letras.
+            if (username.Length > 31)
+            {
+                results.Add(AccountCreationResult.Fail_Username_Too_Long);
+            }
+            //O username deve ter mais que 3 letras
+            if(username.Length < 4)
+            {
+                results.Add(AccountCreationResult.Fail_Username_Too_Short);
+            }
+            //O username não deve estar registrado
+            if(IsRegistered(username))
+            {
+                results.Add(AccountCreationResult.Fail_Account_Already_Exists);
+            }
+            // A password deve conter mais de 7 letras
+            if(passwordUnhashed.Length < 8)
+            {
+                results.Add(AccountCreationResult.Fail_Password_Too_Short);
+            }
+            //A password deve conter menos que 33 letras
+            if(passwordUnhashed.Length > 32)
+            {
+                results.Add(AccountCreationResult.Fail_Password_Too_Long);
+            }
+            //Se existir erros até aqui, retornar
+            if (results.Count > 0) return results;
+
+
+            //Criar a conta
+            string hashedPassword = BCrypt.Net.BCrypt.EnhancedHashPassword(passwordUnhashed);
+            Guid accountID = Guid.NewGuid();
+            DateTime currentTime = DateTime.Now;
+            Account newAccount = new Account(accountID, username, hashedPassword, currentTime);
+
+            //Guardar a conta na BD
+            _accountsContext.Add(newAccount);
+            try
+            {
+                _accountsContext.SaveChanges();
+                results.Add(AccountCreationResult.Success);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao criar a conta de {username}: {ex.Message} " );
+                results.Add(AccountCreationResult.Fail);
+            }
+
+            return results;
+           
+        }
+
+
+        public
     }
 }

83fd780c48837f680ba04a9a00459d1f57f1a6bd
Bonnie20402 <49130379+Bonnie20402@users.noreply.github.com>
2025-10-31 22:15:18 +0000

    Migrações Iniciais

---
+++ OpenRPReloaded/Migrations/20251031201257_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenRPReloaded.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(maxLength: 32, nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    Disabled = table.Column<bool>(nullable: false),
+                    Banned = table.Column<bool>(nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
